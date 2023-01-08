f3f9a3c077adb6550afcad3303324a18033452e3
daniel <DanRhysMorris@gmail.com>
2023-01-08 00:50:58 +0000

    Delete Iterator code since it breaks in release

--- test/Main.cpp
+++ test/Main.cpp
@@ -10,7 +10,6 @@
 
 #include "Standard/Assert.hpp"
 #include "Standard/Math/Math.hpp"
-#include "Standard/Iter/STLWrapper.hpp"
 #include "Standard/Net/Endpoint.hpp"
 #include "Standard/Net/Socket/TCPClient.hpp"
 #include "Standard/Net/Socket/TLSClient.hpp"
@@ -77,76 +76,6 @@ namespace Test
 
 
 
-	void Iterators()
-	{
-		using namespace Strawberry::Standard::Iter;
-
-
-		std::vector<int> numbers{1, 2, 3, 4, 5};
-
-		STLWrapper wrap(numbers);
-		Assert(wrap.Next() == 1);
-		Assert(wrap.Next() == 2);
-		Assert(wrap.Next() == 3);
-		Assert(wrap.Next() == 4);
-		Assert(wrap.Next() == 5);
-
-		auto twice = STLWrapper(numbers).Map<int>([](int x) { return 2 * x; });
-		Assert(twice.Next() == 2);
-		Assert(twice.Next() == 4);
-		Assert(twice.Next() == 6);
-		Assert(twice.Next() == 8);
-		Assert(twice.Next() == 10);
-
-		auto even = STLWrapper(numbers).Filter([](int x) { return x % 2 == 0; });
-		Assert(even.Next() == 2);
-		Assert(even.Next() == 4);
-
-		auto sixes = STLWrapper(numbers)
-				.Map<int>([](int x) { return 3 * x; })
-				.Map<int>([](int x) { return 2 * x; });
-		Assert(sixes.Next() == 6);
-		Assert(sixes.Next() == 12);
-
-
-		auto twelves = STLWrapper(numbers)
-				.Map<int>([](int x) { return 3 * x;} )
-				.Map<int>([](int x) { return 4 * x;} );
-		auto half_twelves = twelves.Map<int>([](int x) { return x / 2;});
-		Assert(half_twelves.Next() == 1 * 6);
-		Assert(half_twelves.Next() == 2 * 6);
-		Assert(half_twelves.Next() == 3 * 6);
-		Assert(half_twelves.Next() == 4 * 6);
-		Assert(half_twelves.Next() == 5 * 6);
-
-		auto even_threes = STLWrapper(numbers)
-				.Map<int>([](int x) { return 3 * x; })
-				.Filter([](int x) { return x % 2 == 0; });
-		Assert(even_threes.Next() == 6);
-		Assert(even_threes.Next() == 12);
-
-
-		auto dropped = STLWrapper(numbers).Drop(2);
-		Assert(dropped.Next() == 3);
-		Assert(dropped.Next() == 4);
-		Assert(dropped.Next() == 5);
-
-
-
-		auto fifteens = STLWrapper(numbers).Map<int>([](int x) { return 15 * x; } );
-		auto fifteens_vector = fifteens.Collect<std::vector<int>>();
-		Assert(fifteens_vector == std::vector<int>{15, 30, 45, 60, 75});
-
-		auto pairs = std::vector<std::pair<int, int>>{{1, 2}, {3, 4}, {5, 6}};
-		auto pairs_iter = STLWrapper(pairs);
-		auto pairs_map = pairs_iter.Collect<std::map<int, int>>();
-		Assert(pairs_map[1] == 2);
-		Assert(pairs_map[3] == 4);
-		Assert(pairs_map[5] == 6);
-	}
-
-
-
 	void ParseIP()
 	{
 		auto ip = Net::IPv4Address::Parse("255.255.255.255").Unwrap();
@@ -209,7 +138,7 @@ namespace Test
 int main()
 {
 	Test::Base64();
-	Test::Iterators();
+	// Test::Iterators();
 	Test::ParseIP();
 	Test::DNS();
 	Test::TCP();
