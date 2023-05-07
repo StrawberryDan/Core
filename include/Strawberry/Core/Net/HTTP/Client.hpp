da9d84cffbc3365bd77664a0b61d1e8140962d4a
daniel <DanRhysMorris@gmail.com>
2023-05-07 12:28:02 +0100

    Cleaned up HTTP client code

--- include/Strawberry/Core/Net/HTTP/Client.hpp
+++ include/Strawberry/Core/Net/HTTP/Client.hpp
@@ -16,31 +16,29 @@ namespace Strawberry::Core::Net::HTTP
 	class ClientBase
 	{
 	public:
-		explicit ClientBase(const std::string& hostname, uint16_t port);
-
-
-
-		inline S&& TakeSocket() { return std::move(mSocket); }
-
-
-
-	public:
+		/// Connects to the given endpoint over HTTP
+		ClientBase(const std::string& hostname, uint16_t port);
+		/// Sends an HTTP Request
 		void SendRequest(const Request& request);
+		/// Waits for an HTTP Response
 		Response Receive();
+
+
+		/// Removes and returns the socket of an rvalue HTTP client.
+		inline S TakeSocket() && { return std::move(mSocket); }
 
 
 
 	private:
+		/// Reads a line of input until a newline character from the socket.
 		std::string ReadLine();
+		/// Reads a chunked HTTP payload from the socket.
 		IO::DynamicByteBuffer ReadChunkedPayload();
 
 
 
 	private:
 		S mSocket;
-
-
-
 	};
 
 
--- src/Net/HTTP/Client.cpp
+++ src/Net/HTTP/Client.cpp
@@ -5,16 +5,12 @@
 namespace Strawberry::Core::Net::HTTP
 {
 	HTTPClient::HTTPClient(const std::string& hostname, uint16_t port)
-			: ClientBase<Socket::TCPClient>(hostname, port)
-	{
-
-	};
+		: ClientBase<Socket::TCPClient>(hostname, port)
+	{}
 
 
 
 	HTTPSClient::HTTPSClient(const std::string& hostname, uint16_t port)
-			: ClientBase<Socket::TLSClient>(hostname, port)
-	{
-
-	}
+		: ClientBase<Socket::TLSClient>(hostname, port)
+	{}
 }
