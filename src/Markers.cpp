a3234826db2d24c0656c046b1d497051a4b56130
StrawberryDan <DanRhysMorris@gmail.com>
2023-06-21 09:29:56 +0100

    Add DebugBreak to Markers

--- src/Markers.cpp
+++ src/Markers.cpp
@@ -3,11 +3,29 @@
 
 
 #include <cstdlib>
+#include <csignal>
+
+#if defined(_WIN32)
+	#include <debugapi.h>
+#endif
 
 
 
 namespace Strawberry::Core
 {
+	void DebugBreak()
+	{
+#if defined(_WIN32)
+
+#elif defined(__UNIX) || defined(__APPLE__)
+		std::raise(SIGTRAP);
+#else
+		#warning "Debug Break not defined on this target!";
+#endif
+	}
+
+
+
 	[[noreturn]] void Unreachable()
 	{
 #if __cpp_lib_unreachable
