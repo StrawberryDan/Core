f7f2b9f5bd244b0261ce5b2fb3786036c4d515d7
StrawberryDan <DanRhysMorris@gmail.com>
2023-06-27 18:12:14 +0100

    Refactor Mutex class format for clarity

--- include/Strawberry/Core/Mutex.hpp
+++ include/Strawberry/Core/Mutex.hpp
@@ -16,37 +16,36 @@ namespace Strawberry::Core
 	template<typename T>
 	class MutexGuard
 	{
-		template<typename T2>
-		friend
-		class Mutex;
+		template<typename>
+		friend class Mutex;
 
 	public:
 		using LockType = std::unique_lock<std::recursive_mutex>;
 
-		MutexGuard(const MutexGuard &other) = delete;
+		MutexGuard(const MutexGuard& other) = delete;
 
-		MutexGuard(MutexGuard &&other) = default;
+		MutexGuard(MutexGuard&& other) = default;
 
-		MutexGuard &operator=(const MutexGuard &) = delete;
+		MutexGuard& operator=(const MutexGuard&) = delete;
 
-		MutexGuard &operator=(MutexGuard &&other) noexcept = default;
+		MutexGuard& operator=(MutexGuard&& other) noexcept = default;
 
 
-		inline T &operator*() const
+		inline T& operator*() const
 		{ return *mPayload; }
 
 
-		inline T *operator->() const
+		inline T* operator->() const
 		{ return mPayload; }
 
 
 	private:
-		MutexGuard(std::recursive_mutex &mutex, T *ptr) : mLock(mutex), mPayload(ptr)
+		MutexGuard(std::recursive_mutex& mutex, T* ptr) : mLock(mutex), mPayload(ptr)
 		{}
 
 
 		LockType mLock;
-		T *mPayload;
+		T*       mPayload;
 	};
 
 
@@ -60,12 +59,12 @@ namespace Strawberry::Core
 		{}
 
 
-		Mutex(Mutex &&rhs) requires(std::is_move_constructible_v<T>)
+		Mutex(Mutex&& rhs) requires(std::is_move_constructible_v<T>)
 				: mMutex(), mPayload(std::move(rhs).Take())
 		{}
 
 
-		Mutex &operator=(Mutex &&other)
+		Mutex& operator=(Mutex&& other)
 		requires(std::is_move_assignable_v<T>)
 		{
 			if (this != &other)
@@ -93,7 +92,7 @@ namespace Strawberry::Core
 		{ return {mMutex, &mPayload}; }
 
 
-		Option<MutexGuard<T>> TryLock() &
+		Option<MutexGuard<T>> TryLock()&
 		{
 			std::unique_lock<std::mutex> lk(mMutex, std::defer_lock);
 			if (lk.try_lock())
@@ -107,7 +106,7 @@ namespace Strawberry::Core
 		}
 
 
-		Option<MutexGuard<const T>> TryLock() const &
+		Option<MutexGuard<const T>> TryLock() const&
 		{
 			std::unique_lock<std::mutex> lk(mMutex, std::defer_lock);
 			if (lk.try_lock())
@@ -121,14 +120,14 @@ namespace Strawberry::Core
 		}
 
 
-		Mutex<T> Clone() &
+		Mutex<T> Clone()&
 		{
 			auto lock = Lock();
 			return Mutex<T>(*lock);
 		}
 
 
-		T &&Take() &&
+		T&& Take()&&
 		{
 			auto lock = Lock();
 			return std::move(*lock);
@@ -164,7 +163,7 @@ namespace Strawberry::Core
 
 		template<typename... Ts>
 		requires std::constructible_from<T, Ts...>
-		void Emplace(Ts... ts) &
+		void Emplace(Ts... ts)&
 		{
 			mPayload = std::make_shared<Mutex<T>>(std::forward<Ts>(ts)...);
 		}
@@ -175,7 +174,7 @@ namespace Strawberry::Core
 
 
 		MutexGuard<const T> Lock() const
-		{ return static_cast<const Mutex<T> *>(mPayload.get())->Lock(); }
+		{ return static_cast<const Mutex<T>*>(mPayload.get())->Lock(); }
 
 
 		Option<MutexGuard<T>> TryLock()
