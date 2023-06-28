3445e30ccb6d0d50a87c774026b49a8192c927c6
StrawberryDan <DanRhysMorris@gmail.com>
2023-06-28 19:54:29 +0100

    Remove unused STL-related concepts
    The STLIterable and STLIteratorConstructible concepts have been removed from the Core Concepts header.

--- include/Strawberry/Core/Concepts.hpp
+++ include/Strawberry/Core/Concepts.hpp
@@ -24,23 +24,4 @@ namespace Strawberry::Core
 	    requires std::invocable<F, Args...>;
 	    requires std::is_same_v<R, std::invoke_result_t<F, Args...>>;
 	};
-
-
-
-	template <typename T>
-	concept STLIterable = requires(T t)
-	{
-		requires std::forward_iterator<typename T::iterator>;
-		{ t.begin() } -> std::forward_iterator;
-		{ t.end()   } -> std::forward_iterator;
-		{ t.size()  } -> std::same_as<size_t>;
-	};
-
-
-
-	template <typename T, typename V>
-	concept STLIteratorConstructible = requires (T t)
-	{
-		requires std::constructible_from<T, decltype(std::declval<std::vector<V>>().begin()), decltype(std::declval<std::vector<V>>().end())>;
-	};
 }
