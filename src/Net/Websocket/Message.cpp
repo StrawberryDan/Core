fae0fde114fb21aca3f38fc907555dfcc5d3e35f
StrawberryDan <DanRhysMorris@gmail.com>
2023-06-30 18:36:08 +0100

    Reformat Message.cpp to improve code readability
    Whitespace inconsistencies were adjusted in several sections of the Websocket Message.cpp source file to maintain a uniform coding style. This enhances code readability and reduces the possibility of future merge conflicts related to formatting.

--- src/Net/Websocket/Message.cpp
+++ src/Net/Websocket/Message.cpp
@@ -10,8 +10,8 @@
 namespace Strawberry::Core::Net::Websocket
 {
 	Message::Message(Message::Opcode opcode, Payload payload)
-	    : mOpcode(opcode)
-	    , mPayload(payload)
+		: mOpcode(opcode)
+		, mPayload(payload)
 	{
 
 	}
@@ -19,8 +19,8 @@ namespace Strawberry::Core::Net::Websocket
 
 
 	Message::Message(const std::string& string)
-	    : mOpcode(Opcode::Text)
-	    , mPayload(string.data(), string.data() + string.size())
+		: mOpcode(Opcode::Text)
+		, mPayload(string.data(), string.data() + string.size())
 	{
 
 	}
@@ -34,8 +34,8 @@ namespace Strawberry::Core::Net::Websocket
 
 
 	Message::Message(std::vector<uint8_t> bytes)
-	    : mOpcode(Opcode::Binary)
-	    , mPayload(std::move(bytes))
+		: mOpcode(Opcode::Binary)
+		, mPayload(std::move(bytes))
 	{
 
 	}
@@ -44,49 +44,49 @@ namespace Strawberry::Core::Net::Websocket
 
 	std::string Message::AsString() const
 	{
-	    return std::string(mPayload.data(), mPayload.data() + mPayload.size());
+		return std::string(mPayload.data(), mPayload.data() + mPayload.size());
 	}
 
 
 
 	Result<nlohmann::json, std::string> Message::AsJSON() const
 	{
-	    nlohmann::json json;
-	    switch (mOpcode)
-	    {
-	        case Opcode::Text:
-	        case Opcode::Binary:
-	        {
-	            try
-	            {
-	                json = nlohmann::json::parse(mPayload.begin(), mPayload.end());
-	            }
-	            catch (std::exception& e)
-	            {
-	                return std::string("Parse Error");
-	            }
-	            return Result<nlohmann::json, std::string>::Ok(std::forward<nlohmann::json>(json));
-	        }
+		nlohmann::json json;
+		switch (mOpcode)
+		{
+			case Opcode::Text:
+			case Opcode::Binary:
+			{
+				try
+				{
+					json = nlohmann::json::parse(mPayload.begin(), mPayload.end());
+				}
+				catch (std::exception& e)
+				{
+					return std::string("Parse Error");
+				}
+				return Result<nlohmann::json, std::string>::Ok(std::forward<nlohmann::json>(json));
+			}
 
-	        default:
+			default:
 				DebugBreak();
-	            return std::string("Invalid Message Type");
-	    }
+				return std::string("Invalid Message Type");
+		}
 	}
 
 
 
 	uint16_t Message::GetCloseStatusCode() const
 	{
-	    uint16_t s = static_cast<uint16_t>(mPayload[0]) << 0 | static_cast<uint16_t>(mPayload[1]) << 8;
-	    s = FromBigEndian(s);
-	    return s;
+		uint16_t s = static_cast<uint16_t>(mPayload[0]) << 0 | static_cast<uint16_t>(mPayload[1]) << 8;
+		s = FromBigEndian(s);
+		return s;
 	}
 
 
 
 	void Message::Append(const Message& other)
 	{
-	    mPayload.insert(mPayload.end(), other.mPayload.begin(), other.mPayload.end());
+		mPayload.insert(mPayload.end(), other.mPayload.begin(), other.mPayload.end());
 	}
 }
