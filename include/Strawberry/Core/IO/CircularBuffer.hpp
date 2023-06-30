73341543f70c93be611efbadda36ba6c346f3e2b
StrawberryDan <DanRhysMorris@gmail.com>
2023-06-30 19:28:06 +0100

    Add CircularBuffer to Core IO
    A new class, CircularBuffer, was added to the Core IO subsystem. The implementation file, CircularBuffer.hpp, was created and included in the project's CMakeLists.txt file for the Core subsystem. The CircularBuffer class is templated and uses raw memory operations to manage a dynamic, circular buffer of arbitrary types. This new data structure will help to improve the performance of data intensive operations by reducing unnecessary memory allocations.

---
+++ include/Strawberry/Core/IO/CircularBuffer.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+
+
+#include "Strawberry/Core/Math/Periodic.hpp"
+#include "Strawberry/Core/Uninitialised.hpp"
+#include "Strawberry/Core/Option.hpp"
+
+#include <memory>
+
+
+
+namespace Strawberry::Core::IO
+{
+	template <typename T>
+	class CircularBuffer
+	{
+	public:
+		CircularBuffer()
+			: mData(16)
+			, mHead(Capacity(), 0)
+			, mTail(Capacity(), 0)
+			, mSize(0)
+		{}
+
+
+		void Push(T value)
+		{
+			if (AtCapacity())
+			{
+				std::destroy_at(&mData[*mHead++]);
+			}
+
+			std::construct_at(&*mData[*mTail++], std::move(value));
+			mSize += 1;
+		}
+
+
+		Option<T> Pop()
+		{
+			if (Size() == 0) return {};
+			auto value = std::move(*mData[*mHead]);
+			std::destroy_at(&*mData[*mHead]);
+			mHead++;
+			mSize -= 1;
+			return value;
+		}
+
+
+		size_t Size()       const { return mSize; }
+		bool   Empty()      const { return Size() == 0; }
+		size_t Capacity()   const { return mData.size(); }
+		bool   AtCapacity() const { return Capacity() == Size(); }
+		void   Clear()            { while (!Empty()) Pop(); }
+
+
+	private:
+		std::vector<Uninitialised<T>> mData;
+		Math::DynamicPeriodic<size_t> mHead;
+		Math::DynamicPeriodic<size_t> mTail;
+		size_t                        mSize;
+	};
+}
