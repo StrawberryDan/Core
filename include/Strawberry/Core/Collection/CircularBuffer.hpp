d670cc1d203b688c1f2d83091fbb1e4a7c859d63
StrawberryDan <DanRhysMorris@gmail.com>
2023-07-01 02:31:54 +0100

    Refactored CircularBuffer and DynamicCircularBuffer implementations
    Refactored how CircularBuffer and DynamicCircularBuffer implementations are being handled by the Strawberry::Core::Collection namespace. This was done to reduce redundant complexities and to use more efficient data management structures. Changes include combining definitions and implementations into a single header file, reorganizing attributes, and optimizing operations such as Push and Pop methods. It also includes a dynamic buffer expansion when capacity is reached.

--- include/Strawberry/Core/Collection/CircularBuffer.hpp
+++ include/Strawberry/Core/Collection/CircularBuffer.hpp
@@ -2,76 +2,127 @@
 
 
 
-#include <concepts>
+#include "Strawberry/Core/Math/Periodic.hpp"
 #include "Strawberry/Core/Option.hpp"
+#include "Strawberry/Core/Option.hpp"
+
+#include <memory>
 
 
 
 namespace Strawberry::Core::Collection
 {
-	template <typename T, size_t S> requires std::movable<T> || std::copyable<T>
+	template <typename T>
 	class CircularBuffer
 	{
 	public:
-		CircularBuffer();
-		CircularBuffer(const CircularBuffer& other) requires std::copyable<T>;
-		CircularBuffer& operator=(const CircularBuffer& other) requires std::copyable<T>;
-		CircularBuffer(CircularBuffer&& other) requires std::movable<T>;
-		CircularBuffer& operator=(CircularBuffer&& other) requires std::movable<T>;
+		CircularBuffer()
+			: mData(16)
+			, mHead(Capacity(), 0)
+			, mTail(Capacity(), 0)
+			, mSize(0)
+		{}
 
 
+		void Push(T value)
+		{
+			if (AtCapacity())
+			{
+                mData[*mHead++].Reset();
+			}
 
-		void Push(T object);
-		Option<T> Pop();
-		std::vector<T> PopAll();
+			mData[*(mTail++)].Emplace(std::move(value));
+			if (!AtCapacity()) mSize += 1;
+		}
 
 
+		Option<T> Pop()
+		{
+			if (Size() == 0) return {};
+			auto value = std::move(*mData[*mHead]);
+			mHead++;
+			mSize -= 1;
+			return value;
+		}
 
-		size_t Size() const { return mSize; }
-		size_t Capacity() const { return S; }
-		size_t RemainingCapacity() const { return Capacity() - Size(); }
 
+		size_t Size()       const { return mSize; }
+		bool   Empty()      const { return Size() == 0; }
+		size_t Capacity()   const { return mData.size(); }
+		bool   AtCapacity() const { return Capacity() == Size(); }
+		void   Clear()            { while (!Empty()) Pop(); }
 
 
 	private:
-		size_t mHead, mTail, mSize;
-		std::array<Option<T>, S> mItems;
+		std::vector<Option<T>>        mData;
+		Math::DynamicPeriodic<size_t> mHead;
+		Math::DynamicPeriodic<size_t> mTail;
+		size_t                        mSize;
 	};
 
 
 
-
-
-	template <typename T> requires std::movable<T> || std::copyable<T>
+	template <typename T>
 	class DynamicCircularBuffer
 	{
 	public:
-		DynamicCircularBuffer();
-		DynamicCircularBuffer(const DynamicCircularBuffer& other) requires std::copyable<T>;
-		DynamicCircularBuffer& operator=(const DynamicCircularBuffer& other) requires std::copyable<T>;
-		DynamicCircularBuffer(DynamicCircularBuffer&& other) requires std::movable<T>;
-		DynamicCircularBuffer& operator=(DynamicCircularBuffer&& other) requires std::movable<T>;
+		DynamicCircularBuffer()
+				: mData(16)
+				, mHead(Capacity(), 0)
+				, mTail(Capacity(), 0)
+				, mSize(0)
+		{}
 
 
+		void Push(T value)
+		{
+			if (AtCapacity())
+			{
+				Resize(2 * Capacity());
+			}
 
-		void Push(T object);
-		Option<T> Pop();
-		std::vector<T> PopAll();
+			mData[*(mTail++)].Emplace(std::move(value));
+			if (!AtCapacity()) mSize += 1;
+		}
 
 
+		Option<T> Pop()
+		{
+			if (Size() == 0) return {};
+			auto value = std::move(*mData[*mHead++]);
+			mSize -= 1;
+			return value;
+		}
 
-		size_t Size() const { return mSize; }
-		size_t Capacity() const { return mItems.size(); }
-		size_t RemainingCapacity() const { return Capacity() - Size(); }
 
+		size_t Size()       const { return mSize; }
+		bool   Empty()      const { return Size() == 0; }
+		size_t Capacity()   const { return mData.size(); }
+		bool   AtCapacity() const { return Capacity() == Size(); }
+		void   Clear()            { while (!Empty()) Pop(); }
 
 
 	private:
-		size_t mHead, mTail, mSize;
-		std::vector<Option<T>> mItems;
+		void Resize(size_t newSize)
+		{
+			std::vector<T> newData;
+			newData.reserve(newSize);
+
+			while (!Empty()) newData.emplace_back(std::move(Pop().Unwrap()));
+
+			mHead = Math::DynamicPeriodic(newSize, (size_t)0);
+			mTail = Math::DynamicPeriodic(newSize, (size_t)0);
+			mSize = 0;
+
+			for (auto& value: newData) Push(std::move(value));
+            mData.resize(newSize);
+		}
+
+
+	private:
+		std::vector<Option<T>>        mData;
+		Math::DynamicPeriodic<size_t> mHead;
+		Math::DynamicPeriodic<size_t> mTail;
+		size_t                        mSize;
 	};
 }
-
-
-
-#include "CircularBuffer.inl"
