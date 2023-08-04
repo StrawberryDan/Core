06a916cea22f271d6e16453f3784be5b4dffe940
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-04 15:45:56 +0100

    Implement Greatest Common Divisor

--- include/Strawberry/Core/Math/Math.hpp
+++ include/Strawberry/Core/Math/Math.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
-
+//======================================================================================================================
+//  Includes
+//----------------------------------------------------------------------------------------------------------------------
+#include <concepts>
 #include <type_traits>
+#include <utility>
 
 
 namespace Strawberry::Core::Math
 {
+	template<std::integral T>
+	T GreatestCommonDivisor(T a, T b)
+	{
+		std::pair<T, T> pair(a, b);
+
+		while (pair.second != 0)
+		{
+			pair = {pair.second, pair.first % pair.second};
+		}
+
+		return pair.first;
+	}
+
+
 	template <typename A, typename B>
 	auto CeilDiv(A num, B den) requires ( std::is_integral_v<A> && std::is_integral_v<B> )
 	{
