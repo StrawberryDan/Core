95bf04472c2dba4e83a88b284a88936fe99988bf
StrawberryDan <DanRhysMorris@gmail.com>
2023-08-17 12:51:41 +0100

    Add ConditionVariable to Core/Sync
    A new ConditionVariable class is implemented under the Core/Sync directory. This is added to provide synchronization primitives for controlling the execution of threads. The new class includes methods like Wait, WaitFor, NotifyOne, and NotifyAll. Updates are also made in the Core's CMakeLists to include the new files.

---
+++ include/Strawberry/Core/Sync/ConditionVariable.cpp
@@ -0,0 +1,36 @@
+//======================================================================================================================
+//  Includes
+//----------------------------------------------------------------------------------------------------------------------
+#include "ConditionVariable.hpp"
+
+
+//======================================================================================================================
+//  Class Definitions
+//----------------------------------------------------------------------------------------------------------------------
+namespace Strawberry::Core
+{
+	void ConditionVariable::Wait()
+	{
+		std::unique_lock lk(mMutex);
+		mConditionVariable.wait(lk);
+	}
+
+
+	void ConditionVariable::WaitFor(double seconds)
+	{
+		std::unique_lock lk(mMutex);
+		mConditionVariable.wait_for(lk, std::chrono::duration<double>(seconds));
+	}
+
+
+	void ConditionVariable::NotifyOne()
+	{
+		mConditionVariable.notify_one();
+	}
+
+
+	void ConditionVariable::NotifyAll()
+	{
+		mConditionVariable.notify_all();
+	}
+} // namespace Strawberry::Core
---
+++ include/Strawberry/Core/Sync/ConditionVariable.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+
+//======================================================================================================================
+//  Includes
+//----------------------------------------------------------------------------------------------------------------------
+// Standard Library
+#include <chrono>
+#include <concepts>
+#include <condition_variable>
+#include <mutex>
+
+
+//======================================================================================================================
+//  Class Declaration
+//----------------------------------------------------------------------------------------------------------------------
+namespace Strawberry::Core
+{
+	class ConditionVariable
+	{
+	public:
+		void Wait();
+
+		template <std::predicate P>
+		void Wait(P predicate)
+		{
+			std::unique_lock lk(mMutex);
+			mConditionVariable.wait(mMutex, predicate);
+		}
+
+
+		void WaitFor(double seconds);
+
+		template <std::predicate P>
+		void WaitFor(double seconds, P predicate)
+		{
+			std::unique_lock lk(mMutex);
+			mConditionVariable.wait(mMutex, std::chrono::duration<double>(seconds), predicate);
+		}
+
+
+		void NotifyOne();
+		void NotifyAll();
+
+
+	private:
+		std::mutex              mMutex;
+		std::condition_variable mConditionVariable;
+	};
+} // namespace Strawberry::Core
