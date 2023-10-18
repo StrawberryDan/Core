5db7fdec69f36631a3f3c837e35b9d72185f9653
StrawberryDan <DanRhysMorris@gmail.com>
2023-10-18 13:01:24 +0100

    Reversions Markers.cpp

---
+++ src/Strawberry/Core/Markers.cpp
@@ -0,0 +1,40 @@
+#include "Strawberry/Core/Markers.hpp"
+
+
+#include <csignal>
+
+
+#if defined(_WIN32)
+#include <debugapi.h>
+#endif
+
+
+namespace Strawberry::Core
+{
+	void DebugBreak()
+	{
+#ifndef NDEBUG
+#if defined(_WIN32)
+		__debugbreak();
+#elif defined(__UNIX) || defined(__APPLE__)
+		std::raise(SIGTRAP);
+#else
+#warning "Debug Break not defined on this target!";
+#endif
+#endif
+	}
+
+
+	[[noreturn]] void Unreachable()
+	{
+		DebugBreak();
+
+#if __cpp_lib_unreachable
+		std::unreachable();
+#elif __clang__
+		__builtin_unreachable();
+#else
+		std::abort();
+#endif
+	}
+} // namespace Strawberry::Core
