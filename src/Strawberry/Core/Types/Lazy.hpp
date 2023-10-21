aeb15638833a9326fedc9aed20758b8f549ed64f
StrawberryDan <DanRhysMorris@gmail.com>
2023-10-21 01:11:49 +0100

    Being Lazy class into working condition

--- src/Strawberry/Core/Types/Lazy.hpp
+++ src/Strawberry/Core/Types/Lazy.hpp
@@ -2,8 +2,6 @@
 //======================================================================================================================
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
-// Core
-#include "Strawberry/Core/Util/Flag.hpp"
 // Standard Library
 #include <concepts>
 #include <functional>
@@ -13,41 +11,82 @@
 //----------------------------------------------------------------------------------------------------------------------
 namespace Strawberry::Core
 {
-	template <typename T>
+	template<typename T>
 	class Lazy
 	{
 	public:
-		T* Get()
+		T& Get()
 		{
 			if (!mInitialised)
 			{
-				mPayload = mInitialiser();
-				mInitialised.Set();
+				T payload = mInitialiser();
+				std::destroy_at(&mInitialiser);
+				std::construct_at(&mPayload, std::move(payload));
+				mInitialised = true;
 			}
 
-			return &mPayload;
+			return mPayload;
+		}
+
+
+		const T& Get() const
+		{
+			if (!mInitialised)
+			{
+				T payload = mInitialiser();
+				std::destroy_at(mInitialiser);
+				std::construct_at(mPayload, std::move(payload));
+				mInitialised = true;
+			}
+
+			return mPayload;
+		}
+
+
+	public:
+		Lazy()
+		requires (std::default_initializable<T>)
+			: mInitialiser([]() -> T { return T(); }) {}
+
+
+		template<typename F>
+		explicit Lazy(F f)
+			: mInitialiser(std::move(f)) {}
+
+
+		Lazy(const Lazy& rhs)
+			: mInitialised(std::copyable<T> && rhs.mInitialised)
+		{
+			if (std::copyable<T> && mInitialised)
+			{
+				std::construct_at(&mPayload, rhs.mPayload);
+			}
+			else
+			{
+				std::construct_at(&mInitialiser, rhs.mInitialiser);
+			}
+		}
+
+
+		~Lazy()
+		{
+			if (!mInitialised)
+			{
+				std::destroy_at(&mInitialiser);
+			}
+			else
+			{
+				std::destroy_at(&mPayload);
+			}
 		}
 
 
 	protected:
-		Lazy()
-			requires (std::default_initializable<T>)
-			: mInitialiser([]() -> T { return T(); })
-		{}
-
-		template <typename F>
-			requires requires { F()->T; }
-		explicit Lazy(F f)
-			: mInitialiser(std::move(f))
-		{}
-
-
-	protected:
-		Flag mInitialised;
+		mutable bool mInitialised = false;
 
 		union
 		{
-			T                  mPayload;
+			T mPayload;
 			std::function<T()> mInitialiser;
 		};
 	};
