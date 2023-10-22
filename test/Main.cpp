e6042682017aa8f8035ce1396aa2c47ca43d7fd2
StrawberryDan <DanRhysMorris@gmail.com>
2023-10-22 12:43:55 +0100

    Fix matrix accessors in Core::Test

--- test/Main.cpp
+++ test/Main.cpp
@@ -237,10 +237,10 @@ namespace Test
 	{
 		Matrix<int, 2, 2> m(1, 2,
 							3, 4);
-		Assert(m[0, 0] == 1);
-		Assert(m[1, 0] == 3);
-		Assert(m[0, 1] == 2);
-		Assert(m[1, 1] == 4);
+		Assert(m[0][0] == 1);
+		Assert(m[1][0] == 3);
+		Assert(m[0][1] == 2);
+		Assert(m[1][1] == 4);
 	}
 
 
