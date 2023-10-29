6d2fa6bff3236961968f2ff6b424ecf4701f18cf
StrawberryDan <DanRhysMorris@gmail.com>
2023-10-29 18:24:34 +0000

    Add debugging asserts to Uninitialised

--- src/Strawberry/Core/Types/Uninitialised.hpp
+++ src/Strawberry/Core/Types/Uninitialised.hpp
@@ -17,32 +17,68 @@ namespace Strawberry::Core
 	public:
 		Uninitialised() {}
 
-		Uninitialised(const Uninitialised& rhs) { std::memcpy(&mPayload, &rhs.mPayload, sizeof(T)); }
 
-		Uninitialised& operator=(const Uninitialised& rhs) { std::memcpy(&mPayload, &rhs.mPayload, sizeof(T)); }
+		Uninitialised(const Uninitialised& rhs)
+#if !NDEBUG
+			: mInitialised(rhs.mInitialised)
+#endif // !NDEBUG
+		{
+			std::memcpy(&mPayload, &rhs.mPayload, sizeof(T));
+		}
+
+
+		Uninitialised& operator=(const Uninitialised& rhs)
+		{
+#if !NDEBUG
+			mInitialised = rhs.mInitialised;
+#endif // !NDEBUG
+			std::memcpy(&mPayload, &rhs.mPayload, sizeof(T));
+		}
+
 
 		Uninitialised(Uninitialised&&)           = delete;
 
+
 		Uninitialised operator=(Uninitialised&&) = delete;
 
-		~Uninitialised() {}
+
+		~Uninitialised()
+		{
+#if !NDEBUG
+			Core::Assert(!mInitialised);
+#endif // !NDEBUG
+		}
 
 		template <typename... Args>
 			requires std::constructible_from<T, Args...>
 		void Construct(Args... args)
 		{
 			std::construct_at(&mPayload, std::forward<Args>(args)...);
+#if !NDEBUG
+			Core::Assert(!mInitialised);
+			mInitialised = true;
+#endif // !NDEBUG
 		}
 
-		void Destruct() { std::destroy_at(&mPayload); }
+		void Destruct()
+		{
+#if !NDEBUG
+			static_assert(false);
+			Core::Assert(mInitialised);
+			mInitialised = false;
+#endif // !NDEBUG
+			std::destroy_at(&mPayload);
+		}
 
 		T& Get() { return mPayload; }
 
 		const T& Get() const { return mPayload; }
 
+
 		T& operator*() { return mPayload; }
 
 		const T& operator*() const { return mPayload; }
+
 
 		T* operator->() { return &mPayload; }
 
