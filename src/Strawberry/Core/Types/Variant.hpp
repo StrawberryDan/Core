07b53e42e8ded3f96d1fcd78a55fba12b265989a
StrawberryDan <DanRhysMorris@gmail.com>
2024-01-20 16:02:28 +0000

    Implement AddVariantTypes and MergeVariantTypes

--- src/Strawberry/Core/Types/Variant.hpp
+++ src/Strawberry/Core/Types/Variant.hpp
@@ -17,9 +17,44 @@
 namespace Strawberry::Core
 {
 	template <typename... Types>
+	class Variant;
+
+
+	template <typename V, typename... T>
+	struct AddVariantTypes {};
+
+
+	template <typename... VTs, typename... Ts>
+	struct AddVariantTypes<Variant<VTs...>, Ts...>
+	{
+		using Type = Variant<VTs..., Ts...>;
+	};
+
+
+	template<typename V1, typename V2>
+	struct MergeVariantTypes {};
+
+
+	template <typename... Ts1, typename... Ts2>
+	struct MergeVariantTypes<Variant<Ts1...>, Variant<Ts2...>>
+	{
+		using Type = Variant<Ts1..., Ts2...>;
+	};
+
+
+	template <typename... Types>
 	class Variant
 	{
 	public:
+		template <typename... Ts>
+		using AddTypes = typename AddVariantTypes<Variant<Types...>, Ts...>::Type;
+
+		template <typename... Ts>
+		using MergeTypes = typename MergeVariantTypes<Variant<Types...>, Variant<Ts...>>::Type;
+
+
+
+
 		template <std::convertible_to<std::variant<Types...>> T>
 		Variant(T data)
 			: mData(std::move(data))
