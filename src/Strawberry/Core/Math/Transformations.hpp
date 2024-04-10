e1f5de045145fe69880764c321b6c43b9fc650e5
StrawberryDan <DanRhysMorris@gmail.com>
2024-04-10 10:10:55 +0100

    Implement Orthographic Matrix function

--- src/Strawberry/Core/Math/Transformations.hpp
+++ src/Strawberry/Core/Math/Transformations.hpp
@@ -42,4 +42,14 @@ namespace Strawberry::Core::Math
 	{
 		return Scale(Core::Math::Vector<T, sizeof...(Ts)>(args...));
 	}
+
+
+	template <typename T>
+	Matrix<T, 4, 4> Orthographic(float left, float right, float top, float bottom, float near, float far)
+	{
+		return Matrix<T, 4, 4>(2.0 / (right - left), 0.0,                   0.0,                -(right + left) / (right - left),
+							   0.0,                  2.0 / (top - bottom),  0.0,                -(top + bottom) / (top - bottom),
+							   0.0,                  0.0,                  -2.0 / (far - near), -(far + near)   / (far - near),
+							   0.0,                  0.0,                   0.0,                 1.0);
+	}
 }
