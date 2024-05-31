f6efa3d3793ff65b84fd7de97c21bd3f1f477f88
StrawberryDan <DanRhysMorris@gmail.com>
2024-05-31 13:11:54 +0100

    Add Core::Unreachable for values

--- src/Strawberry/Core/Markers.hpp
+++ src/Strawberry/Core/Markers.hpp
@@ -6,4 +6,10 @@ namespace Strawberry::Core
 	void DebugBreak();
 
 	[[noreturn]] void Unreachable();
+
+	template<typename T>
+	[[noreturn]] T Unreachable()
+	{
+		Unreachable();
+	}
 } // namespace Strawberry::Core
