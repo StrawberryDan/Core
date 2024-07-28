0ad421a6d262e998f7779e21801e459141727ea0
StrawberryDan <DanRhysMorris@gmail.com>
2024-07-28 18:48:01 +0100

    Improve Assert Functions

--- src/Strawberry/Core/Assert.hpp
+++ src/Strawberry/Core/Assert.hpp
@@ -12,30 +12,27 @@
 
 namespace Strawberry::Core
 {
-	inline constexpr void Assert([[maybe_unused]] bool value)
+	constexpr void Assert([[maybe_unused]] bool value)
 	{
+		static_assert(STRAWBERRY_DEBUG);
 #if STRAWBERRY_DEBUG
 		if (!value)
 		{
 			DebugBreak();
-		}
-		else
-		{
-			return;
 		}
 #endif // STRAWBERRY_DEBUG
 	}
 
 
 	template<typename A, typename B>
-	inline constexpr void AssertEQ([[maybe_unused]] A a, [[maybe_unused]] B b)
+	constexpr void AssertEQ([[maybe_unused]] A a, [[maybe_unused]] B b)
 	{
 		Assert(a == b);
 	}
 
 
 	template<typename A, typename B>
-	inline constexpr void AssertNEQ([[maybe_unused]] A a, [[maybe_unused]] B b)
+	constexpr void AssertNEQ([[maybe_unused]] A a, [[maybe_unused]] B b)
 	{
 		Assert(a != b);
 	}
