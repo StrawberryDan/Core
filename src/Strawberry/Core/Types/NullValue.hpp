16efece3d5d9efd333436b0984a054ddc9e715c3
StrawberryDan <DanRhysMorris@gmail.com>
2024-10-05 13:46:22 +0100

    Add constructors and operators to NullValue template class
    Implemented default and parameterized constructors for NullValue, enabling initialization with a value. Added explicit boolean and implicit type conversion operators for better usability and integration.

--- src/Strawberry/Core/Types/NullValue.hpp
+++ src/Strawberry/Core/Types/NullValue.hpp
@@ -12,7 +12,31 @@
 namespace Strawberry::Core
 {
 	template<std::integral T, T Null>
-	class NullValue {};
+	class NullValue
+	{
+	public:
+		NullValue() = default;
+
+
+		NullValue(T value)
+			: mValue(value) {}
+
+
+		explicit operator bool() const noexcept
+		{
+			return mValue != Null;
+		}
+
+
+		operator T() const noexcept
+		{
+			return mValue;
+		}
+
+
+	private:
+		T mValue = Null;
+	};
 
 
 	template<std::integral T>
