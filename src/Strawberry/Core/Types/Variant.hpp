b05d320c4159cdb765e0a9a62eef29eb32455230
StrawberryDan <DanRhysMorris@gmail.com>
2025-01-19 16:05:32 +0000

    Implement my own Variant class

--- src/Strawberry/Core/Types/Variant.hpp
+++ src/Strawberry/Core/Types/Variant.hpp
@@ -8,7 +8,8 @@
 #include "Optional.hpp"
 // Standard Library
 #include <concepts>
-#include <variant>
+#include <functional>
+#include <unordered_map>
 
 
 //======================================================================================================================
@@ -16,152 +17,276 @@
 //----------------------------------------------------------------------------------------------------------------------
 namespace Strawberry::Core
 {
-	template<typename... Types>
+	// Type Aliases
+	using TypeIndex = size_t;
+	static constexpr TypeIndex TypeNotFound = std::numeric_limits<TypeIndex>::max();
+
+	// Predeclarations
+	//
+	// Variant type
+	template<typename... Ts>
 	class Variant;
+	// Helper struct which calculates the variant type that is the union of the types given.
+	template<typename V1, typename... V2>
+	struct VariantUnion {};
+	template <typename V>
+	struct VariantSize {};
+	template <typename V>
+	struct VariantAlignment {};
 
+	template <TypeIndex, typename...>
+	struct VariantTypeIndexImpl {};
 
-	template<typename V, typename... T>
-	struct AddVariantTypes {};
-
-
-	template<typename... VTs, typename... Ts>
-	struct AddVariantTypes<Variant<VTs...>, Ts...>
+	template <TypeIndex I, typename Subject, typename A> requires (!std::same_as<Subject, A>)
+	struct VariantTypeIndexImpl<I, Subject, A>
 	{
-		using Type = Variant<VTs..., Ts...>;
+		static constexpr TypeIndex Value = TypeNotFound;
+	};
+
+	template <TypeIndex I, typename Subject, typename A> requires (std::same_as<Subject, A>)
+	struct VariantTypeIndexImpl<I, Subject, A>
+	{
+		static constexpr TypeIndex Value = I;
+	};
+
+	template <TypeIndex I, typename Subject, typename A, typename... Ts> requires(!std::same_as<Subject, A>)
+	struct VariantTypeIndexImpl<I, Subject, A, Ts...>
+	{
+		static constexpr TypeIndex Value = VariantTypeIndexImpl<I + 1, Subject, Ts...>::Value;
+	};
+
+	template <TypeIndex I, typename Subject, typename A, typename... Ts> requires std::same_as<Subject, A>
+	struct VariantTypeIndexImpl<I, Subject, A, Ts...>
+	{
+		static constexpr TypeIndex Value = I;
+	};
+
+	template <typename Subject, typename... Ts>
+	using VariantTypeIndex =  VariantTypeIndexImpl<0, std::decay_t<Subject>, std::decay_t<Ts>...>;
+
+
+
+	// Helper Struct Implementations
+	//
+	// VariantUnion implementation for one variant on the left
+	template<typename... Left, typename... Right>
+	struct VariantUnion<Variant<Left...>, Right...>
+	{
+		using Type = Variant<Left..., Right...>;
 	};
 
 
-	template<typename V1, typename V2>
-	struct MergeVariantTypes {};
-
-
-	template<typename... Ts1, typename... Ts2>
-	struct MergeVariantTypes<Variant<Ts1...>, Variant<Ts2...>>
+	// Variant Union Implementation for 2 variants.
+	template<typename... Left, typename... Right>
+	struct VariantUnion<Variant<Left...>, Variant<Right...>>
 	{
-		using Type = Variant<Ts1..., Ts2...>;
+		using Type = Variant<Left..., Right...>;
 	};
 
 
+	template <typename... Ts>
+	struct VariantSize<Variant<Ts...>>
+	{
+		static constexpr TypeIndex Value = std::max({sizeof(Ts)...});
+	};
+
+
+	template <typename... Ts>
+	struct VariantAlignment<Variant<Ts...>>
+	{
+		static constexpr TypeIndex Value = std::max({alignof(Ts)...});
+	};
+
+
+	// Helper struct calculating whether a type is part of a variant.
 	template<typename T, typename... Ts>
 	struct IsInVariant
 			: std::false_type {};
 
 
-	template<typename T, typename... Ts> requires (std::same_as<T, Ts> || ...)
+	// Specialization of IsInVariant for when a type is inside a variant.
+	template<typename T, typename... Ts> requires (std::same_as<std::decay_t<T>, Ts> || ...)
 	struct IsInVariant<T, Variant<Ts...>>
 			: std::true_type {};
 
 
-	template<typename... Types>
+
+
+
+	// Variant Class Declaration
+	template<typename... Ts>
 	class Variant
 	{
 	public:
-		template<typename... Ts>
-		using AddTypes = typename AddVariantTypes<Variant<Types...>, Ts...>::Type;
-
-		template<typename... Ts>
-		using MergeTypes = typename MergeVariantTypes<Variant<Types...>, Variant<Ts...>>::Type;
+		template<typename... Additional>
+		using Union = typename VariantUnion<Variant, Additional...>::Type;
 
 
-		template<std::convertible_to<std::variant<Types...>> T>
-		Variant(T data)
-			: mData(std::move(data)) {}
-
-
-		[[nodiscard]] bool ContainsValue() const
+		template<typename Arg> requires IsInVariant<std::decay_t<Arg>, Variant>::value
+		Variant(Arg&& arg)
+			: mTypeIndex(VariantTypeIndex<Arg, Ts...>::Value)
 		{
-			return !mData.valueless_by_exception();
+			std::construct_at(reinterpret_cast<std::add_pointer_t<std::decay_t<Arg>>>(mData), std::forward<Arg>(arg));
 		}
 
 
-		template<typename T>
+		~Variant()
+		{
+
+		}
+
+
+		template<typename Arg>
 		[[nodiscard]] bool IsType() const
 		{
-			return std::holds_alternative<T>(mData);
+			return mTypeIndex == VariantTypeIndex<Arg, Ts...>::Value;
 		}
 
 
-		template<typename T>
-		Core::Optional<T> Take()
+		template<typename Arg>
+		Optional<Arg> Take()
 		{
-			Core::Assert(ContainsValue());
-			if (std::holds_alternative<T>(mData))
+			if (IsType<Arg>()) [[likely]]
 			{
-				return std::move(std::get<T>(std::move(mData)));
+				return std::move(*reinterpret_cast<Arg*>(mData));
 			}
-			else
+			return NullOpt;
+		}
+
+
+		template<typename Arg>
+		Arg& Ref()
+		{
+			Core::Assert(IsType<Arg>());
+			return *reinterpret_cast<Arg*>(mData);
+		}
+
+
+		template<typename Arg>
+		const Arg& Ref() const
+		{
+			Core::Assert(IsType<Arg>());
+			return *reinterpret_cast<const Arg*>(mData);
+		}
+
+
+		template<typename Arg>
+		Optional<Arg*> Ptr() &
+		{
+			if (IsType<Arg>())
 			{
-				return Core::NullOpt;
+				return reinterpret_cast<Arg*>(mData);
 			}
+
+			return NullOpt;
 		}
 
 
-		template<typename T> requires (std::copyable<T>)
-		Core::Optional<T> Value() const
+		template<typename Arg>
+		Optional<const Arg*> Ptr() const &
 		{
-			Core::Assert(ContainsValue());
-			if (std::holds_alternative<T>(mData))
+			if (IsType<Arg>())
 			{
-				return std::get<T>(mData);
+				return reinterpret_cast<const Arg*>(mData);
 			}
-			else
-			{
-				return Core::NullOpt;
-			}
-		}
 
-
-		template<typename T>
-		T& Ref()
-		{
-			return std::get<T>(mData);
-		}
-
-
-		template<typename T>
-		const T& Ref() const
-		{
-			return std::get<T>(mData);
-		}
-
-
-		template<typename T>
-		Core::Optional<T*> Ptr()
-		{
-			Core::Assert(ContainsValue() && IsType<T>());
-			return std::get_if<T>(&mData);
-		}
-
-
-		template<typename T>
-		Core::Optional<const T*> Ptr() const &
-		{
-			Core::Assert(ContainsValue() && IsType<T>());
-			return static_cast<const T*>(std::get_if<T>(&mData));
-		}
-
-
-		template<typename T> requires (IsInVariant<T, Variant>::value)
-		bool operator==(const Variant& other) const
-		{
-			return IsType<T>() && Ref<T>() == other;
-		}
-
-
-		template<typename T> requires (IsInVariant<T, Variant>::value)
-		bool operator!=(const Variant& other) const
-		{
-			return !IsType<T>() || Ref<T>() != other;
+			return NullOpt;
 		}
 
 
 		template <typename F>
-		auto Visit(F&& function)
+		decltype(auto) Visit(F&& functor)
 		{
-			return std::visit(std::forward<F>(function), mData);
+			using Arg = typename std::tuple_element<0, std::tuple<Ts...>>::type;
+			using Ret = std::invoke_result_t<F, Arg>;
+
+			using FN  = Ret(*)(void*, F&&);
+			static constexpr FN sDispatchTable[] = { [](void* data, F&& functor) -> Ret { return std::invoke(functor, *static_cast<Ts*>(data)); }... };
+
+			return std::invoke(sDispatchTable[mTypeIndex], reinterpret_cast<void*>(mData), std::forward<F>(functor));
 		}
 
+
+		template <typename T>
+		bool operator==(const T& other) const noexcept
+		{
+			using Fn = bool(*)(const void*, const void*);
+
+			static constexpr Fn fns[] = {
+				[](const void* pA, const void* pB)
+				{
+					const Ts& a = *static_cast<const Ts*>(pA);
+					const T&  b = *static_cast<const T* >(pB);
+
+					if constexpr (std::equality_comparable_with<Ts, T>)
+					{
+						return a == b;
+					}
+
+					return false;
+				}...
+			};
+
+			return std::invoke(fns[mTypeIndex], mData, &other);
+		}
+
+
+		bool operator==(const Variant& other) const noexcept requires (std::equality_comparable<Ts> && ...)
+		{
+			using Fn = bool(*)(const void*, const void*);
+
+			static constexpr Fn fns[] = { [](const void* a, const void* b)
+			{
+				return *static_cast<const Ts*>(a) == *static_cast<const Ts*>(b);
+			}... };
+
+			return std::invoke(fns[mTypeIndex], mData, other.mData);
+		}
+
+
+		template <typename T>
+		bool operator!=(const T& other) const noexcept
+		{
+			using Fn = bool(*)(const void*, const void*);
+
+			static constexpr Fn fns[] = {
+				[](const void* pA, const void* pB)
+				{
+					const Ts& a = *static_cast<const Ts*>(pA);
+					const T&  b = *static_cast<const T* >(pB);
+
+					if constexpr (std::equality_comparable_with<Ts, T>)
+					{
+						return a != b;
+					}
+
+					return true;
+				}...
+			};
+
+			return std::invoke(fns[mTypeIndex], mData, &other);
+		}
+
+
+		bool operator!=(const Variant& other) const noexcept requires (std::equality_comparable<Ts> && ...)
+		{
+			using Fn = bool(*)(const void*, const void*);
+
+			static constexpr Fn fns[] = { [](const void* a, const void* b)
+			{
+				return *static_cast<const Ts*>(a) != *static_cast<const Ts*>(b);
+			}... };
+
+			return std::invoke(fns[mTypeIndex], mData, other.mData);
+		}
+
+
 	private:
-		std::variant<Types...> mData;
+		static constexpr size_t Size = VariantSize<Variant>::Value;
+		static constexpr size_t Alignment = VariantAlignment<Variant>::Value;
+
+
+		size_t                     mTypeIndex;
+		alignas(Alignment) uint8_t mData[Size];
 	};
 } // namespace Strawberry::Core
