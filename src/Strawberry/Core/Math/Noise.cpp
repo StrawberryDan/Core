e8f39257eb6d24e13bbb478acf630579c0a35e70
StrawberryDan <DanRhysMorris@gmail.com>
2025-03-10 17:04:40 +0000

    Implement Noise Adapters

--- src/Strawberry/Core/Math/Noise.cpp
+++ src/Strawberry/Core/Math/Noise.cpp
@@ -1,22 +1,20 @@
 #include "Noise.hpp"
 
+#include "Math.hpp"
 
-namespace Strawberry::Core::Math
+
+namespace Strawberry::Core::Math::Noise
 {
-	LinearNoise::LinearNoise(uint32_t seed, float period)
+	Linear::Linear(int seed, float period)
 			: mSeed(seed)
 			, mPeriod(period)
 	{}
 
 
-	float LinearNoise::WhiteIntegerNoise(Vec2u position) const
+	float Linear::WhiteIntegerNoise(Vec2i position) const
 	{
-		// Decompose input parameter
-		const auto& [x, y] = position;
-		// Calculate Cantor Pairing of these integers.
-		uint32_t cantor = (x + y) * (x + y + 1) / 2 + y;
 		// Seed a mersenne twister engine with this pairing combined with the seed.
-		std::mt19937 engine(mSeed xor cantor);
+		std::mt19937_64 engine(mSeed xor position.Embed());
 		// Sample the real distribution between -1.0 and 1.0 with this engine.
 		std::uniform_real_distribution<float> distribution(-1.0, 1.0);
 		float sample = distribution(engine);
@@ -25,7 +23,7 @@ namespace Strawberry::Core::Math
 	}
 
 
-	float LinearNoise::operator()(Vec2f position) const noexcept
+	float Linear::operator()(Vec2f position) const noexcept
 	{
 		// Decompose input coordinates.
 		const auto& [x, y] = position;
@@ -33,16 +31,16 @@ namespace Strawberry::Core::Math
 		const float waveX = std::floor(x / mPeriod);
 		const float waveY = std::floor(y / mPeriod);
 		// Calculate the phase of the input point relative to the period.
-		const float phaseX = std::fmod(x, mPeriod);
-		const float phaseY = std::fmod(y, mPeriod);
+		const float phaseX = waveX >= 0 ? std::fmod(x, mPeriod) : mPeriod + std::fmod(x, mPeriod);
+		const float phaseY = waveY >= 0 ? std::fmod(y, mPeriod) : mPeriod + std::fmod(y, mPeriod);
 		// Calculate the ratio between the phase and the period
 		const float phaseRatioX = phaseX / mPeriod;
 		const float phaseRatioY = phaseY / mPeriod;
 		// Calculate the four noise corner values.
 		const float grid[2][2]
 		{
-			{ WhiteIntegerNoise({waveX + 0, waveY + 0}), WhiteIntegerNoise({waveX + 1, waveY + 0}) },
-			{ WhiteIntegerNoise({waveX + 0, waveY + 1}), WhiteIntegerNoise({waveX + 1, waveY + 1}) }
+			{ WhiteIntegerNoise(Vec2i{waveX + 0, waveY + 0}), WhiteIntegerNoise(Vec2i{waveX + 1, waveY + 0}) },
+			{ WhiteIntegerNoise(Vec2i{waveX + 0, waveY + 1}), WhiteIntegerNoise(Vec2i{waveX + 1, waveY + 1}) }
 		};
 		// Calculate Bilinear interpolation between these points.
  		const float a = std::lerp(grid[0][0], grid[0][1], phaseRatioX);
--- src/Strawberry/Core/Math/Noise.hpp
+++ src/Strawberry/Core/Math/Noise.hpp
@@ -4,14 +4,14 @@
 #include <random>
 
 
-namespace Strawberry::Core::Math
+namespace Strawberry::Core::Math::Noise
 {
 	// Class representing a signal of white noise interpolated over a given period.
-	class LinearNoise
+	class Linear
 	{
 	public:
 		// Creates a new noise signal with the given seed and period.
-		LinearNoise(uint32_t seed, float period);
+		Linear(int seed, float period);
 
 
 		// Returns the value of this noise signal at the given position.
@@ -20,12 +20,126 @@ namespace Strawberry::Core::Math
 
 	private:
 		// Returns a consistent random value in [-1.0, 1.0] for any input coordinate
-		float WhiteIntegerNoise(Vec2u position) const;
+		float WhiteIntegerNoise(Vec2i position) const;
 
 
 		// The seed for this signal
-		uint32_t mSeed;
+		unsigned int mSeed;
 		// The orthogonal distance between two white noise values in input space
 		float    mPeriod;
 	};
+
+
+	namespace Adapter
+	{
+		template <typename Base>
+		class Rotate
+		{
+		public:
+			Rotate(Base&& base, float orientation)
+				: mBase(std::forward<Base>(base))
+			{
+				mOrientation = Core::Math::Mat2f{
+					std::cosf(orientation), -std::sinf(orientation),
+					std::sinf(orientation), std::cosf(orientation)};
+			}
+
+
+			Rotate(Base&& base)
+				: mBase(std::forward<Base>(base))
+			{
+				std::uniform_real_distribution<float> distribution(0.0f, 2.0f * M_PI);
+
+				std::random_device random;
+				float orientation = distribution(random);
+				mOrientation = Core::Math::Mat2f{
+					std::cosf(orientation), -std::sinf(orientation),
+					std::sinf(orientation), std::cosf(orientation)};
+			}
+
+
+			float operator()(Vec2f position) const
+			{
+				return mBase(mOrientation * position);
+			}
+
+
+		private:
+			Mat2f mOrientation;
+			Base mBase;
+		};
+
+
+		template <typename Base>
+		class Scale
+		{
+		public:
+			Scale(float amplitude, Base&& base)
+				: mAmplitude(amplitude)
+				, mBase(std::forward<Base>(base))
+			{}
+
+
+			float Amplitude() const noexcept
+			{
+				return mAmplitude;
+			}
+
+
+			float operator()(Vec2f position) const
+			{
+				return mAmplitude * mBase(position);
+			}
+
+
+		private:
+			float mAmplitude;
+			Base mBase;
+		};
+
+
+		template <typename Base>
+		class Layer
+		{
+		public:
+			template <typename F> requires std::same_as<std::decay_t<std::invoke_result_t<F, unsigned int>>, Base>
+			Layer(const unsigned int layerCount, F&& generator)
+			{
+				for (unsigned int i = 0; i < layerCount; i++)
+				{
+					mSignals.emplace_back(generator(i));
+				}
+			}
+
+
+			auto begin(this auto& self)
+			{
+				return self.mSignals.begin();
+			}
+
+			auto end(this auto& self)
+			{
+				return self.mSignals.end();
+			}
+
+
+			float operator()(Vec2f position) const
+			{
+				float value = 0.0f;
+
+				for (auto&& signal : mSignals)
+				{
+					value += signal(position);
+				}
+
+				return value;
+			}
+		private:
+			std::vector<Base> mSignals;
+		};
+
+
+		template <typename T>
+		Layer(unsigned int, T&&) -> Layer<std::decay_t<std::invoke_result_t<T, unsigned int>>>;
+	}
 }
