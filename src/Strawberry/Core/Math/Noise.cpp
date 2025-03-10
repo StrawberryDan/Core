23cc7da38387eeca3fb30d89348518fdb143bb0b
StrawberryDan <DanRhysMorris@gmail.com>
2025-03-10 11:54:02 +0000

    Comment LinearNoise class

--- src/Strawberry/Core/Math/Noise.cpp
+++ src/Strawberry/Core/Math/Noise.cpp
@@ -3,55 +3,52 @@
 
 namespace Strawberry::Core::Math
 {
-    LinearNoise::LinearNoise(uint32_t seed, float period)
-            : mSeed(seed)
-            , mPeriod(period)
-    {}
+	LinearNoise::LinearNoise(uint32_t seed, float period)
+			: mSeed(seed)
+			, mPeriod(period)
+	{}
 
 
-    float LinearNoise::Noise(Core::Math::Vec2u position) const
-    {
-        const auto& [x, y] = position;
-
-        uint32_t cantor = (x + y) * (x + y + 1) / 2 + y;
-
-        std::mt19937 engine(mSeed xor cantor);
-
-        const float randomSample = engine();
-        constexpr float MAXIMUM = std::numeric_limits<uint32_t>::max();
-
-        return randomSample / MAXIMUM;
-    }
-
-
-    float LinearNoise::operator()(Vec2f position) const noexcept
-    {
-        const auto& [x, y] = position;
+	float LinearNoise::WhiteIntegerNoise(Vec2u position) const
+	{
+		// Decompose input parameter
+		const auto& [x, y] = position;
+		// Calculate Cantor Pairing of these integers.
+		uint32_t cantor = (x + y) * (x + y + 1) / 2 + y;
+		// Seed a mersenne twister engine with this pairing combined with the seed.
+		std::mt19937 engine(mSeed xor cantor);
+		// Sample the real distribution between -1.0 and 1.0 with this engine.
+		std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+		float sample = distribution(engine);
+		// Return our sample.
+		return sample;
+	}
 
 
-        const float waveX = std::floor(x / mPeriod);
-        const float waveY = std::floor(y / mPeriod);
+	float LinearNoise::operator()(Vec2f position) const noexcept
+	{
+		// Decompose input coordinates.
+		const auto& [x, y] = position;
+		// Calculate the index of the wave that the input point is inside.
+		const float waveX = std::floor(x / mPeriod);
+		const float waveY = std::floor(y / mPeriod);
+		// Calculate the phase of the input point relative to the period.
+		const float phaseX = std::fmod(x, mPeriod);
+		const float phaseY = std::fmod(y, mPeriod);
+		// Calculate the ratio between the phase and the period
+		const float phaseRatioX = phaseX / mPeriod;
+		const float phaseRatioY = phaseY / mPeriod;
+		// Calculate the four noise corner values.
+		const float grid[2][2]
+		{
+			{ WhiteIntegerNoise({waveX + 0, waveY + 0}), WhiteIntegerNoise({waveX + 1, waveY + 0}) },
+			{ WhiteIntegerNoise({waveX + 0, waveY + 1}), WhiteIntegerNoise({waveX + 1, waveY + 1}) }
+		};
+		// Calculate Bilinear interpolation between these points.
+ 		const float a = std::lerp(grid[0][0], grid[0][1], phaseRatioX);
+		const float b = std::lerp(grid[1][0], grid[1][1], phaseRatioX);
+		const float c = std::lerp(a, b, phaseRatioY);
 
-
-        const float phaseX = std::fmod(x, mPeriod);
-        const float phaseY = std::fmod(y, mPeriod);
-
-
-        const float phaseRatioX = phaseX / mPeriod;
-        const float phaseRatioY = phaseY / mPeriod;
-
-
-        const float grid[2][2]
-        {
-            {Noise({waveX + 0, waveY + 0}), Noise({waveX + 1, waveY + 0})},
-            {Noise({waveX + 0, waveY + 1}), Noise({waveX + 1, waveY + 1})}
-        };
-
-
-        const float a = std::lerp(grid[0][0], grid[0][1], phaseRatioX);
-        const float b = std::lerp(grid[1][0], grid[1][1], phaseRatioX);
-        const float c = std::lerp(a, b, phaseRatioY);
-
-        return c;
-    }
+		return c;
+	}
 }
--- src/Strawberry/Core/Math/Noise.hpp
+++ src/Strawberry/Core/Math/Noise.hpp
@@ -6,20 +6,26 @@
 
 namespace Strawberry::Core::Math
 {
+	// Class representing a signal of white noise interpolated over a given period.
 	class LinearNoise
 	{
 	public:
+		// Creates a new noise signal with the given seed and period.
 		LinearNoise(uint32_t seed, float period);
 
-	private:
-		float Noise(Core::Math::Vec2u position) const;
 
-	public:
+		// Returns the value of this noise signal at the given position.
 		float operator()(Vec2f position) const noexcept;
 
 
 	private:
+		// Returns a consistent random value in [-1.0, 1.0] for any input coordinate
+		float WhiteIntegerNoise(Vec2u position) const;
+
+
+		// The seed for this signal
 		uint32_t mSeed;
+		// The orthogonal distance between two white noise values in input space
 		float    mPeriod;
 	};
 }
