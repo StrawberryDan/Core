6f9010e52eaea30aa52b52727f8ccf4e6f399ad2
StrawberryDan <DanRhysMorris@gmail.com>
2025-05-08 14:47:02 +0100

    Simplify RepeatingTask

--- src/Strawberry/Core/Thread/RepeatingTask.hpp
+++ src/Strawberry/Core/Thread/RepeatingTask.hpp
@@ -3,17 +3,15 @@
 //======================================================================================================================
 //  Includes
 //----------------------------------------------------------------------------------------------------------------------
+#include "Strawberry/Core/Types/Optional.hpp"
 #include <atomic>
 #include <functional>
 #include <thread>
-
-#include "Strawberry/Core/Types/ReflexivePointer.hpp"
 
 
 namespace Strawberry::Core
 {
 	class RepeatingTask
-			: public EnableReflexivePointer
 	{
 	public:
 		/// Accepts a function taking no arguments.
@@ -27,65 +25,15 @@ namespace Strawberry::Core
 
 
 		/// Accepts a function taking no arguments.
-		explicit RepeatingTask(std::function<void()> function, std::function<void()> startup)
+		explicit RepeatingTask(
+			std::function<void()> function,
+			Optional<std::function<void>> startUp = NullOpt,
+			Optional<std::function<void>> shutDown = NullOpt
+		)
 			: mShouldRun(false)
 			, mStartUp(std::move(startup))
+			, mShutdown(std::move(shutDown))
 			, mFunction(std::move(function))
-		{
-			Start();
-		}
-
-
-		/// Will pass 'this' as the first argument always.
-		explicit RepeatingTask(std::function<void(RepeatingTask*)> function)
-			: mShouldRun(false)
-			, mStartUp()
-			, mFunction([function, this]
-			{
-				function(this);
-			})
-		{
-			Start();
-		}
-
-
-		/// Accepts a function taking no arguments.
-		explicit RepeatingTask(std::function<void(RepeatingTask*)> startup, std::function<void()> function)
-			: mShouldRun(false)
-			, mStartUp([startup, this]
-			{
-				startup(this);
-			})
-			, mFunction(std::move(function))
-		{
-			Start();
-		}
-
-
-		/// Will pass 'this' as the first argument always.
-		explicit RepeatingTask(std::function<void()> startup, const std::function<void(RepeatingTask*)> function)
-			: mShouldRun(false)
-			, mStartUp(std::move(startup))
-			, mFunction([function, this]
-			{
-				function(this);
-			})
-		{
-			Start();
-		}
-
-
-		/// Will pass 'this' as the first argument always.
-		explicit RepeatingTask(std::function<void(RepeatingTask*)> startup, std::function<void(RepeatingTask*)> function)
-			: mShouldRun(false)
-			, mStartUp([startup, this]
-			{
-				startup(this);
-			})
-			, mFunction([function, this]
-			{
-				function(this);
-			})
 		{
 			Start();
 		}
@@ -106,17 +54,21 @@ namespace Strawberry::Core
 		void Start()
 		{
 			mShouldRun = true;
-			mThread    = std::thread([self = GetReflexivePointer()]()
+			mThread    = std::thread([this]
 			{
-				if (self->mStartUp)
+				if (mStartUp)
 				{
-					(*self->mStartUp)();
-					self->mStartUp.Reset();
+					std::invoke(mStartUp.Value());
 				}
 
 				while (self->mShouldRun)
 				{
 					self->mFunction();
+				}
+
+				if (mShutdown)
+				{
+					std::invoke(mShutdown.Value());
 				}
 			});
 		}
@@ -134,43 +86,15 @@ namespace Strawberry::Core
 
 		RepeatingTask(const RepeatingTask& rhs)            = delete;
 		RepeatingTask& operator=(const RepeatingTask& rhs) = delete;
+		RepeatingTask(RepeatingTask&& rhs) = delete;
+		RepeatingTask& operator=(RepeatingTask&& rhs) = delete;
 
-
-		RepeatingTask(RepeatingTask&& rhs)
-		{
-			bool wasRunning = rhs.IsRunning();
-			if (rhs.IsRunning())
-			{
-				rhs.Stop();
-			}
-
-			mShouldRun = false;
-			mThread    = std::move(rhs.mThread);
-			mStartUp   = std::move(rhs.mStartUp);
-			mFunction  = std::move(rhs.mFunction);
-
-			if (wasRunning)
-			{
-				Start();
-			}
-		}
-
-
-		RepeatingTask& operator=(RepeatingTask&& rhs)
-		{
-			if (this != &rhs)
-			{
-				std::destroy_at(this);
-				std::construct_at(this, std::forward<RepeatingTask>(rhs));
-			}
-
-			return *this;
-		}
 
 	private:
 		std::atomic<bool>               mShouldRun;
 		std::thread                     mThread;
 		Optional<std::function<void()>> mStartUp;
+		Optional<std::function<void()>> mShutdown;
 		std::function<void()>           mFunction;
 	};
 } // namespace Strawberry::Core
