1049b031cb31b2f75b2c0266f2c86185c3f4edf4
dmorris <DanRhysMorris@gmail.com>
2025-08-02 16:40:38 +0100

    Correct matrix test after fixing access operator

--- test/Matrices.cpp
+++ test/Matrices.cpp
@@ -7,12 +7,10 @@ using namespace Strawberry::Core::Math;
 
 int main()
 {
-    Matrix<int, 2, 2> m(1,
-                        2,
-                        3,
-                        4);
+    Matrix<int, 2, 2> m(1, 2,
+                        3, 4);
     Assert(m[0][0] == 1);
-    Assert(m[1][0] == 3);
-    Assert(m[0][1] == 2);
+    Assert(m[1][0] == 2);
+    Assert(m[0][1] == 3);
     Assert(m[1][1] == 4);
 }
