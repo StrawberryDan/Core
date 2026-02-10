7aba502f61415b4425bc0a4c57851dbfc6a336d6
dmorris <DanRhysMorris@gmail.com>
2026-02-10 21:21:19 +0000

    Tabify Checked.hpp

--- src/Strawberry/Core/Math/Checked.hpp
+++ src/Strawberry/Core/Math/Checked.hpp
@@ -3,20 +3,20 @@
 
 namespace Strawberry::Core::Math
 {
-    template <typename T>
-    struct CheckedResult
-    {
+	template <typename T>
+	struct CheckedResult
+	{
 	bool overflow;
-	T    value;
+	T	 value;
 
 	explicit operator bool() const noexcept { return overflow; };
 	T operator*() const noexcept { return value; }
-    };
+	};
 
 
-    template <typename T>
-    constexpr CheckedResult<T> CheckedAddition(T a, T b) noexcept
-    {
+	template <typename T>
+	constexpr CheckedResult<T> CheckedAddition(T a, T b) noexcept
+	{
 #if STRAWBERRY_COMPILER_GCC || STRAWBERRY_COMPILER_CLANG
 	CheckedResult<T> result;
 	result.overflow = __builtin_add_overflow(a, b, &result.value);
@@ -24,11 +24,11 @@ namespace Strawberry::Core::Math
 #else
 #error "No implementation for CheckedAddition for this compiler"
 #endif
-    }
+	}
 
-    template <typename T>
-    constexpr CheckedResult<T> CheckedSubtraction(T a, T b) noexcept
-    {
+	template <typename T>
+	constexpr CheckedResult<T> CheckedSubtraction(T a, T b) noexcept
+	{
 #if STRAWBERRY_COMPILER_GCC || STRAWBERRY_COMPILER_CLANG
 	CheckedResult<T> result;
 	result.overflow = __builtin_sub_overflow(a, b, &result.value);
@@ -36,17 +36,17 @@ namespace Strawberry::Core::Math
 #else
 #error "No implementation for CheckedSubtraction for this compiler"
 #endif
-    }
+	}
 
-    template <typename T>
-    constexpr CheckedResult<T> CheckedMultiplication(T a, T b) noexcept
-    {
+	template <typename T>
+	constexpr CheckedResult<T> CheckedMultiplication(T a, T b) noexcept
+	{
 #if STRAWBERRY_COMPILER_GCC || STRAWBERRY_COMPILER_CLANG
 	CheckedResult<T> result;
-        result.overflow = __builtin_mul_overflow(a, b, &result.value);
+		result.overflow = __builtin_mul_overflow(a, b, &result.value);
 	return result;
 #else
 #error "No implementation for CheckedMultiplication for this compiler"
 #endif
-    }
+	}
 }
