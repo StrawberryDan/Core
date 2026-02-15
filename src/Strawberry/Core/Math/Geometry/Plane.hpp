47c2258e9e5ae5e7936621fa6cdb4e86391598fc
dmorris <DanRhysMorris@gmail.com>
2026-02-15 20:23:19 +0000

    Add Plane.hpp

---
+++ src/Strawberry/Core/Math/Geometry/Plane.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+
+#include "Strawberry/Core/Math/Vector.hpp"
+
+
+
+namespace Strawberry::Core::Math
+{
+	template <typename T, unsigned D>
+	class Plane
+	{
+	public:
+		/// Returns the plane corresponding to the triangle formed by 3 counter-clockwise points.
+		static Plane FromTriangle(const Vector<T, D>& a, const Vector<T, D>& b, const Vector<T, D>& c)
+		{
+			Vector<T, D> normal = (b - a).Cross(c - a).Normalised();
+			T dotProduct = normal.Dot(a);
+			return Plane(normal, dotProduct);
+		}
+
+
+		Plane(const Vector<T, D>& normal, T dotProduct)
+			: mNormal(normal) , mDotProduct(dotProduct) {}
+
+
+	private:
+		Vector<T, D> mNormal;
+		T            mDotProduct;
+	};
+}
