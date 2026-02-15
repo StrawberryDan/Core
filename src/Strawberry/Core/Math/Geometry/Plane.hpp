c185081e2e12ee9bd1bf32abc60b707a62f72f1f
dmorris <DanRhysMorris@gmail.com>
2026-02-15 20:59:17 +0000

    Make Plane Constructible from Triangle

--- src/Strawberry/Core/Math/Geometry/Plane.hpp
+++ src/Strawberry/Core/Math/Geometry/Plane.hpp
@@ -2,7 +2,7 @@
 
 
 #include "Strawberry/Core/Math/Vector.hpp"
-
+#include "Strawberry/Core/Math/Geometry/Simplex.hpp"
 
 
 namespace Strawberry::Core::Math
@@ -12,6 +12,12 @@ namespace Strawberry::Core::Math
 	{
 	public:
 		/// Returns the plane corresponding to the triangle formed by 3 counter-clockwise points.
+		static Plane FromTriangle(const Triangle<T, D>& triangle)
+		{
+			return FromTriangle(triangle.Point(0), triangle.Point(1), triangle.Point(2));
+		}
+
+
 		static Plane FromTriangle(const Vector<T, D>& a, const Vector<T, D>& b, const Vector<T, D>& c)
 		{
 			Vector<T, D> normal = (b - a).Cross(c - a).Normalised();
