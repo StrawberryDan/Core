07ad7ffc09e3013f07164de6266bae5981358382
dmorris <DanRhysMorris@gmail.com>
2026-02-16 17:32:56 +0000

    Further implement Delauney and add drawing logic to test

--- src/Strawberry/Core/Math/Geometry/Delauney.hpp
+++ src/Strawberry/Core/Math/Geometry/Delauney.hpp
@@ -1,26 +1,149 @@
 #pragma once
 
 
+#include "Strawberry/Core/Markers.hpp"
 #include "Strawberry/Core/Math/Geometry/PointSet.hpp"
+#include "Strawberry/Core/Math/Geometry/Simplex.hpp"
 #include "Strawberry/Core/Math/Graph.hpp"
+#include <algorithm>
+#include <ranges>
 
 
 namespace Strawberry::Core::Math
 {
 	template <typename T>
-	class Delauney : public GraphTypeUndirected<Vector<T, 2>>
+	class Delauney : public UndirectedGraph<Vector<T, 2>>
 	{
 	public:
+		using Graph = UndirectedGraph<Vector<T, 2>>;
+		using Edge = typename Graph::Edge;
+
+
+		struct Face
+		{
+			Face(unsigned int a, unsigned int b, unsigned int c)
+				: mFaces{a, b, c}
+			{
+				std::sort(mFaces.begin(), mFaces.end());
+			}
+
+			auto operator<=>(const Face& other) const = default;
+
+			std::array<unsigned int, 3> mFaces;
+		};
+
+
 		static Delauney FromPoints(const PointSet<T, 2>& points)
 		{
 			Delauney delaunay;
 
-			
+			auto min = points.MinExtreme();
+			auto max = points.MaxExtreme();
+			min = min + 0.1 * (min - max);
+			max = max + 0.1 * (max - min);
+
+			delaunay.Graph::AddNode(min);
+			delaunay.Graph::AddNode(Vector{max[0], min[1]});
+			delaunay.Graph::AddNode(Vector{min[0], max[1]});
+			delaunay.Graph::AddNode(max);
+
+			delaunay.AddEdge(0, 1);
+			delaunay.AddEdge(1, 2);
+			delaunay.AddEdge(2, 0);
+			delaunay.AddEdge(1, 3);
+			delaunay.AddEdge(2, 3);
+
+			for (int i = 0; i < points.Size(); i++)
+			{
+				delaunay.AddNode(points.Get(i));
+			}
 
 			return delaunay;
 		}
 
 
+		Vector<T, 2> GetMinPoint() const
+		{
+			return Graph::GetValue(0);
+		}
+
+
+		Vector<T, 2> GetMaxPoint() const
+		{
+			return Graph::GetValue(3);
+		}
+
+
+		void AddNode(const Vector<T, 2>& value)
+		{
+			const auto& min = GetMinPoint();
+			const auto& max = GetMaxPoint();
+			Core::Assert(min[0] <= value[0] && value[0] <= max[0]);
+			Core::Assert(min[1] <= value[1] && value[1] <= max[1]);
+
+			unsigned int newNode = Graph::AddNode(value);
+
+			Face containingFace = FindContainingFace(value);
+
+			AddEdge(newNode, containingFace.mFaces[0]);
+			AddEdge(newNode, containingFace.mFaces[1]);
+			AddEdge(newNode, containingFace.mFaces[2]);
+		}
+
+
+		void AddEdge(unsigned int a, unsigned int b)
+		{
+			AddEdge(Edge(a, b));
+		}
+
+
+		void AddEdge(const Edge& edge)
+		{
+			Graph::AddEdge(edge);
+
+			auto neighboursA = this->GetNeighbourIndices(edge.nodes[0]);
+			auto neighboursB = this->GetNeighbourIndices(edge.nodes[1]);
+
+			std::set<unsigned int> mutualNeighbours;
+
+			std::set_intersection(neighboursA.begin(), neighboursA.end(),
+								  neighboursB.begin(), neighboursB.end(),
+								  std::inserter(mutualNeighbours, mutualNeighbours.begin()));
+
+			for (auto mn : mutualNeighbours)
+			{
+				mFaces.emplace(edge.nodes[0], edge.nodes[1], mn);
+			}
+		}
+
+
+		Face FindContainingFace(const Vector<T, 2>& value)
+		{
+			for (auto face : std::views::reverse(mFaces))
+			{
+				Triangle<T, 2> triangle(this->GetValue(face.mFaces[0]), this->GetValue(face.mFaces[1]), this->GetValue(face.mFaces[2]));
+				triangle.MakeCounterClockwise();
+
+				Core::Logging::Trace("Checking point {}, {} against triangle ({}, {}), ({}, {}),.({}, {}) -> {}",
+									 value[0], value[1],
+									 triangle.Point(0)[0], triangle.Point(0)[1],
+									 triangle.Point(1)[0], triangle.Point(1)[1],
+									 triangle.Point(2)[0], triangle.Point(2)[1],
+									 triangle.Contains(value)
+			);
+
+				if (triangle.Contains(value))
+					return face;
+			}
+
+			Core::Unreachable();
+		}
+
+
 		Delauney() = default;
+
+
+	private:
+		std::set<Face> mFaces;
 	};
 }
--- test/Delauney.cpp
+++ test/Delauney.cpp
@@ -1,4 +1,6 @@
 #include "Strawberry/Core/Math/Geometry/Delauney.hpp"
+#include "Strawberry/Core/Util/Image.hpp"
+#include "canvas_ity.hpp"
 
 
 using namespace Strawberry::Core;
@@ -8,7 +10,48 @@ int main()
 {
 	PointSet<double, 2> pointSet;
 
+	pointSet.Add({0.0, 0.0});
+	pointSet.Add({1000.0, 1000.0});
+	pointSet.Add({800, 900});
+	pointSet.Add({900, 800});
+	pointSet.Add({700, 800});
+	pointSet.Add({800, 700});
+
 	auto delauney = Delauney<double>::FromPoints(pointSet);
+
+	auto min = delauney.GetMinPoint();
+	auto max = delauney.GetMaxPoint();
+	auto span = max - min;
+	span[0] += 20; span[1] += 20;
+
+	Image<PixelRGBA> image(span.AsType<unsigned int>());
+
+	canvas_ity::canvas context(span[0], span[1]);
+
+	for (auto nodes : delauney.GetNodes())
+	{
+		auto pos = nodes.second.Offset(10, 10) - min;
+		Logging::Info("Drawing node at {}, {}", pos[0], pos[1]);
+		context.set_line_width(5.0);
+		context.set_color(canvas_ity::brush_type::stroke_style, 1.0f, 1.0f, 1.0f, 1.0f);
+		context.begin_path();
+		context.arc(pos[0], pos[1], 5, 0, 360);
+		context.stroke();
+	}
+
+	for (auto edge : delauney.GetEdges())
+	{
+		Vector<double, 2> posA = delauney.GetValue(edge.nodes[0]).Offset(10, 10) - min;
+		Vector<double, 2> posB = delauney.GetValue(edge.nodes[1]).Offset(10, 10) - min;
+		context.move_to(posA[0], posA[1]);
+		context.line_to(posB[0], posB[1]);
+		context.stroke();
+	}
+
+	context.get_image_data((unsigned char*) image.Data(), image.Width(), image.Height(), image.Width() * decltype(image)::PixelType::Size, 0, 0);
+
+
+	image.Save("delauney_output.png");
 
 	return 0;
 }
