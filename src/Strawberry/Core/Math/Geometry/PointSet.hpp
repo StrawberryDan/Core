fa57a8195541e65bf42c6bb92c4f6750788b1b19
dmorris <DanRhysMorris@gmail.com>
2026-02-16 13:19:00 +0000

    Add functions Min/Max extreme to PointSet

--- src/Strawberry/Core/Math/Geometry/PointSet.hpp
+++ src/Strawberry/Core/Math/Geometry/PointSet.hpp
@@ -2,6 +2,7 @@
 
 
 #include "Strawberry/Core/Math/Vector.hpp"
+#include <algorithm>
 
 
 namespace Strawberry::Core::Math
@@ -13,9 +14,44 @@ namespace Strawberry::Core::Math
 		PointSet() = default;
 
 
+		const Vector<T, D> Get(unsigned int i) const { return mPoints[i]; }
+		Vector<T, D>& Get(unsigned int i) { return mPoints[i]; }
+
+
+		unsigned int Size() const { return mPoints.size(); }
+
+
 		void Add(const Vector<T, D>& point)
 		{
 			mPoints.push_back(point);
+		}
+
+
+		Vector<T, D> MinExtreme() const
+		{
+			Vector<T, D> min;
+			for (auto point : mPoints)
+			{
+				for (int d = 0; d < D; d++)
+				{
+					min[d] = std::min(min[d], point[d]);
+				}
+			}
+			return min;
+		}
+
+
+		Vector<T, D> MaxExtreme() const
+		{
+			Vector<T, D> max;
+			for (auto point : mPoints)
+			{
+				for (int d = 0; d < D; d++)
+				{
+					max[d] = std::max(max[d], point[d]);
+				}
+			}
+			return max;
 		}
 
 
