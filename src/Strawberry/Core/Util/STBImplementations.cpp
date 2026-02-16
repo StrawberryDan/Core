c2d15359249243f4af3c1af4f76ae176557ed60d
dmorris <DanRhysMorris@gmail.com>
2026-02-16 17:31:06 +0000

    Fix STBImplementations.cpp

--- src/Strawberry/Core/Util/STBImplementations.cpp
+++ src/Strawberry/Core/Util/STBImplementations.cpp
@@ -1,6 +1,6 @@
 // STB
 #define STB_IMAGE_IMPLEMENTATION
-#define STBI_WRITE_NO_STDIO
+// #define STBI_WRITE_NO_STDIO
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
