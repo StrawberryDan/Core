684efdc1e2c0183bf8a017224bd1f927d64a7246
dmorris <DanRhysMorris@gmail.com>
2026-02-18 15:18:09 +0000

    Move Ray to Line.hpp

--- src/Strawberry/Core/Math/Geometry/Line.hpp
+++ src/Strawberry/Core/Math/Geometry/Line.hpp
@@ -6,7 +6,73 @@
 
 namespace Strawberry::Core::Math
 {
-	template <typename T, size_t D>
+	template <typename T, unsigned int D>
+	class Line;
+	template <typename T, unsigned int D>
+	class LineSegment;
+	template <typename T, unsigned int D>
+	class Ray;
+
+
+	template <typename T, unsigned D>
+	class Ray
+	{
+	public:
+		Ray() = default;
+		Ray(const Vector<T, D>& origin, const Vector<T, D>& direction)
+			: mOrigin(origin), mDirection(direction.Normalised()) {}
+
+
+		Vector<T, D>& Origin() { return mOrigin; }
+		const Vector<T, D>& Origin() const { return mOrigin; }
+
+
+		void SetDirection(const Vector<T, D>& direction) { mDirection = direction.Normalised(); }
+		const Vector<T, D>& Direction() const { return mDirection; }
+
+
+		Line<T, D> IntoLine() const
+		{
+			return Line(mOrigin, mOrigin + mDirection);
+		}
+
+
+		Optional<Vector<T, D>> Intersection(const Ray& other) const noexcept
+		{
+			// Solve using Cramer's rule
+			auto& p1 = this->Origin();
+			auto& p2 = other.Origin();
+			auto   c = p2 - p1;
+			auto& v1 = this->Direction();
+			auto& v2 = other.Direction();
+
+			double determinant = v1[0] * v2[1] - v2[0] - v1[1];
+			if (determinant == 0.0)
+			{
+				return NullOpt;
+			}
+
+			auto t1Num = c[0] * v2[1] - v2[0] * c[1];
+			auto t1 = t1Num / determinant;
+			auto t2Num = v1[0] * c[1] - c[0] * v1[1];
+			auto t2 = t2Num / determinant;
+
+			if (t1 < 0.0 || t2 < 0.0)
+			{
+				return NullOpt;
+			}
+
+			return p1 + t1 * v1;
+		}
+
+
+	private:
+		Vector<T, D> mOrigin;
+		Vector<T, D> mDirection;
+	};
+
+
+	template <typename T, unsigned int D>
 	class Line
 	{
 	public:
