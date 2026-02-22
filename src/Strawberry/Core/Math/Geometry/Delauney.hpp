9def6d0ee404bb16c4fdd99c5bdcab62abccc34e
dmorris <DanRhysMorris@gmail.com>
2026-02-22 15:56:05 +0000

    Add Face::ContainsEdge

--- src/Strawberry/Core/Math/Geometry/Delauney.hpp
+++ src/Strawberry/Core/Math/Geometry/Delauney.hpp
@@ -43,6 +43,12 @@ namespace Strawberry::Core::Math
 			}
 
 
+			bool ContainsEdge(Edge e) const noexcept
+			{
+				return GetEdges().contains(e);
+			}
+
+
 			std::set<Edge> GetEdges() const noexcept
 			{
 				return {
