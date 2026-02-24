d4fdcfbe399adae74f5fc974ecc681ced778fc2b
dmorris <DanRhysMorris@gmail.com>
2026-02-24 01:07:16 +0000

    Implement cell tracking for Voronoi

--- src/Strawberry/Core/Math/Geometry/Voronoi.hpp
+++ src/Strawberry/Core/Math/Geometry/Voronoi.hpp
@@ -12,6 +12,12 @@ namespace Strawberry::Core::Math
 	class Voronoi
 	{
 	public:
+		struct Cell
+		{
+			std::vector<unsigned int> nodes;
+		};
+
+
 		/// Returns the voronoi edge graph for this delaunay triangulation.
 		///
 		/// This works because delauney triangulations and voronoi diagrams are duals of eachother.
@@ -23,9 +29,11 @@ namespace Strawberry::Core::Math
 
 			UndirectedGraph<Vector<T, 2>> voronoi;
 
-
 			std::map<Face, unsigned int> faceCenterNodeIDs;
-
+			/// Mapping of triangulation node  handles to the list
+			/// of edge graph node handles that make up the cell surrounding this
+			/// point.
+			std::map<unsigned int, Cell> cellMap;
 
 			for (auto face : delauney.Faces())
 			{
@@ -42,8 +50,24 @@ namespace Strawberry::Core::Math
 				}
 			}
 
+			for (auto node : delauney.Nodes())
+			{
+				auto containingFaces = delauney.Faces()
+					| std::views::filter([&] (Face face) { return face.ContainsNode(node); })
+					| std::views::transform([&] (Face face) { return faceCenterNodeIDs.at(face); })
+					| std::ranges::to<std::vector>();
 
-			return Voronoi(delauney.PruneSupportingVertices(), std::move(voronoi));
+
+				std::ranges::sort(containingFaces, std::less{}, [&] (auto face)
+				{
+					return (voronoi.GetValue(face) - delauney.GetValue(node)).ATan2();
+				});
+
+				cellMap[node] = Cell { .nodes = containingFaces };
+			}
+
+
+			return Voronoi(delauney, std::move(voronoi), std::move(cellMap));
 		}
 
 
@@ -52,13 +76,29 @@ namespace Strawberry::Core::Math
 
 
 	private:
-		Voronoi(UndirectedGraph<Vector<T, 2>> triangulation, UndirectedGraph<Vector<T, 2>> voronoi) noexcept
+		Voronoi(UndirectedGraph<Vector<T, 2>> triangulation,
+				UndirectedGraph<Vector<T, 2>> voronoi,
+				std::map<unsigned int, Cell> cellMap) noexcept
 			: mTriangulation(std::move(triangulation))
-			, mVoronoi(std::move(voronoi)) {}
+			, mVoronoi(std::move(voronoi))
+			, mCellMap(std::move(cellMap))
+		{
+			// Assert(IsPlanar(), "Result of Voronoi constructor was not planar!");
+		}
+
+
+		bool IsPlanar() const noexcept
+		{
+			return Cells().Nodes().size() - Cells().Edges().size() + mCellMap.size() == 1;
+		}
 
 
 	private:
 		UndirectedGraph<Vector<T, 2>> mTriangulation;
 		UndirectedGraph<Vector<T, 2>> mVoronoi;
+		/// A map of indices of nodes in the triangulation mapped to lists
+		/// of cell indices in the voronoi mapping which form the cell conainting
+		/// that index.
+		std::map<unsigned int, Cell> mCellMap;
 	};
 }
