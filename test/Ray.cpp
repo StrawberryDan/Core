cf35ee1582bf78e8b683ecc2fdd32a78eb6d0449
dmorris <DanRhysMorris@gmail.com>
2026-02-28 16:26:16 +0000

    Add test/Ray.cpp

---
+++ test/Ray.cpp
@@ -0,0 +1,25 @@
+#include "Strawberry/Core/Math/Geometry/Ray.hpp"
+
+
+using namespace Strawberry::Core;
+using namespace Math;
+
+
+int main()
+{
+	Ray<double, 2> r1 ({0, 0}, {1, 1});
+	Ray<double, 2> r2({0, -1}, {-1, 1});
+	Ray<double, 2> r3({1, 0}, {-1, 1});
+
+
+	LineSegment<double, 2> ls1({10, 0}, {10, 10});
+
+	Line<double, 2> l1({-10, 0}, {0, 1});
+
+	AssertEQ(r1.Intersection(r3), Vector{0.5, 0.5});
+	Assert(!r1.Intersection(r2));
+	AssertEQ(r1.Intersection(ls1), Vector{10., 10.});
+	AssertEQ(r2.Intersection(l1), Vector{-10., 9.});
+
+	return 0;
+}
