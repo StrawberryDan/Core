45e99f58dd3bb4d92f68c51c30c2be725d8836e5
dmorris <DanRhysMorris@gmail.com>
2026-04-28 13:52:25 +0100

    Implement Simplex Area for 2D Triangles

--- src/Strawberry/Core/Math/Geometry/Simplex.hpp
+++ src/Strawberry/Core/Math/Geometry/Simplex.hpp
@@ -53,6 +53,15 @@ namespace Strawberry::Core::Math
 		}
 
 
+		[[nodiscard]] double Area() const noexcept requires (Order == 3 && Dimension == 2)
+		{
+			return 0.5 * std::abs(
+				Point(0)[0] * (Point(1)[1] - Point(2)[1]) +
+				Point(1)[0] * (Point(2)[1] - Point(0)[1]) +
+				Point(2)[0] * (Point(0)[1] - Point(1)[1]));
+		}
+
+
 		bool IsCounterClockwise() const requires (Dimension == 2 && Order == 3)
 		{
 			auto a = Point(1) - Point(0);
